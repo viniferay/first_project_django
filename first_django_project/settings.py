719942420385bac95c67e7175f196a8949725074
Vinicius Ferreira <vinicius.feray@gmail.com>
2018-08-30 00:38:10 -0300

    Add app widget-tweaks

--- first_django_project/settings.py
+++ first_django_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Aplicações do Vini
+    #Apps da biblioteca
+    'widget_tweaks',
+    #Aplicações do projeto
     'core',
     'tarefas',
 ]
